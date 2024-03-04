88967b45f0f1d1895c4c0c3a0edc11a3049ba4a2
Enrico Weigelt, metux IT consult <info@metux.net>
2024-03-04 20:02:18 +0100

    include: move dbus-core.h to config
    This header is internal (not installed) and holds definitions for sources
    in config/, thus it fells more clean moving it to config/, too.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1357>
    (cherry picked from commit 8982344e53cb5954a65fdf0e95d17a7f2bc6142a)

--- hw/xfree86/common/xf86Init.c
+++ hw/xfree86/common/xf86Init.c
@@ -48,12 +48,14 @@
 #include <X11/Xmd.h>
 #include <X11/Xproto.h>
 #include <X11/Xatom.h>
+
+#include "config/dbus-core.h"
+
 #include "input.h"
 #include "servermd.h"
 #include "windowstr.h"
 #include "scrnintstr.h"
 #include "mi.h"
-#include "dbus-core.h"
 #include "systemd-logind.h"
 
 #include "loaderProcs.h"
