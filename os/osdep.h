external-mirrors/xorg-xserver

commit 1642adec3b44beef7cee196525394ef27053faf5

dix: unexport Ones()
It's not used by any module/driver, so no need to keep it exported.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1695>

os/osdep.h
@@ -215,4 +215,6 @@ extern Bool CoreDump;
 extern Bool NoListenAll;
 extern Bool AllowByteSwappedClients;
 
+int Ones(unsigned long mask);
+
 #endif                          /* _OSDEP_H_ */

Xext/panoramiXprocs.c
@@ -32,6 +32,7 @@ Equipment Corporation.
 #include <X11/Xproto.h>
 
 #include "dix/dix_priv.h"
+#include "os/osdep.h"
 
 #include "windowstr.h"
 #include "dixfontstr.h"

Xext/saver.c
@@ -36,6 +36,7 @@ in this Software without prior written authorization from the X Consortium.
 #include "dix/colormap_priv.h"
 #include "dix/cursor_priv.h"
 #include "dix/dix_priv.h"
+#include "os/osdep.h"
 #include "os/screensaver.h"
 
 #include "misc.h"
@@ -61,6 +62,10 @@ in this Software without prior written authorization from the X Consortium.
 #endif
 #include "protocol-versions.h"
 #include "extinit_priv.h"
+
+// temporary workaround for win32/mingw32 name clash
+// see: https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1355
+#undef CreateWindow
 
 static int ScreenSaverEventBase = 0;
 

Xext/security.c
@@ -35,6 +35,7 @@ in this Software without prior written authorization from The Open Group.
 #include "include/extinit_priv.h"
 #include "os/audit.h"
 #include "os/auth.h"
+#include "os/osdep.h"
 
 #include "scrnintstr.h"
 #include "inputstr.h"

Xext/sync.c
@@ -59,6 +59,7 @@ PERFORMANCE OF THIS SOFTWARE.
 #include <X11/extensions/syncproto.h>
 
 #include "dix/dix_priv.h"
+#include "os/osdep.h"
 
 #include "scrnintstr.h"
 #include "os.h"

composite/compinit.c
@@ -44,6 +44,7 @@
 #include <dix-config.h>
 
 #include "dix/dix_priv.h"
+#include "os/osdep.h"
 
 #include "compint.h"
 #include "compositeext.h"

dix/devices.c
@@ -60,6 +60,7 @@ SOFTWARE.
 #include "dix/exevents_priv.h"
 #include "dix/input_priv.h"
 #include "dix/ptrveloc_priv.h"
+#include "os/osdep.h"
 #include "xkb/xkbsrv_priv.h"
 
 #include "misc.h"

dix/gc.c
@@ -52,6 +52,7 @@ SOFTWARE.
 #include <X11/Xproto.h>
 
 #include "dix/gc_priv.h"
+#include "os/osdep.h"
 
 #include "misc.h"
 #include "resource.h"

commit 061690c2e649ce41ae277dd7555ad90855376e31

Re-export Ones()
It's used by the nvidia driver

Fixes: 1642adec3 ("dix: unexport Ones()")
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2051>

os/osdep.h
@@ -205,7 +205,7 @@ extern Bool NoListenAll;
 extern Bool AllowByteSwappedClients;
 
 #if __has_builtin(__builtin_popcountl)
-# define Ones __builtin_popcountl
+# define Xpopcountl __builtin_popcountl
 #else
 /*
  * Count the number of bits set to 1 in a 32-bit word.
@@ -213,7 +213,7 @@ extern Bool AllowByteSwappedClients;
  * https://dspace.mit.edu/handle/1721.1/6086
  */
 static inline int
-Ones(unsigned long mask)
+Xpopcountl(unsigned long mask)
 {
     unsigned long y;
 
@@ -222,5 +222,6 @@ Ones(unsigned long mask)
     return (((y + (y >> 3)) & 030707070707) % 077);
 }
 #endif
+#define Ones Xpopcountl
 
 #endif                          /* _OSDEP_H_ */

os/utils.c
@@ -1563,3 +1563,13 @@ AbortServer(void)
         OsAbort();
     exit(1);
 }
+
+/* Exported version for binary compatibility with modules which call it */
+#undef Ones
+_X_EXPORT int Ones(unsigned long);
+
+int
+Ones (unsigned long mask)
+{
+    return Xpopcountl(mask);
+}