letoram/xarcan

commit d36f66f15d367c86cb93605caa2a6520a2fcde46

xwayland: Check for duplicate output names
Even though the name provided by either xdg-output or wl_output are
guaranteed to be unique, that might not be the case with output names
between different protocols, such as the one offered for DRM lease.

To avoid running into name conflicts, check that no other existing
output of the same name exists prior to changing the output name.

Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1492>

hw/xwayland/xwayland-output.c
@@ -687,6 +687,9 @@ void
 xwl_output_set_name(struct xwl_output *xwl_output, const char *name)
 {
     struct xwl_screen *xwl_screen = xwl_output->xwl_screen;
+    rrScrPrivPtr pScrPriv;
+    RRLeasePtr lease;
+    int i;
 
     if (xwl_output->randr_output == NULL)
         return; /* rootful */
@@ -695,6 +698,24 @@ xwl_output_set_name(struct xwl_output *xwl_output, const char *name)
     if (!name || !strlen(name)) {
         ErrorF("Not using the provided output name, invalid");
         return;
+    }
+
+    /* Check for duplicate names to be safe */
+    pScrPriv = rrGetScrPriv(xwl_screen->screen);
+    for (i = 0; i < pScrPriv->numOutputs; i++) {
+        if (!strcmp(name, pScrPriv->outputs[i]->name)) {
+            ErrorF("An output named '%s' already exists", name);
+            return;
+        }
+    }
+    /* And leases' names as well */
+    xorg_list_for_each_entry(lease, &pScrPriv->leases, list) {
+        for (i = 0; i < lease->numOutputs; i++) {
+            if (!strcmp(name, pScrPriv->outputs[i]->name)) {
+                ErrorF("A lease output named '%s' already exists", name);
+                return;
+            }
+        }
     }
 
     snprintf(xwl_output->randr_output->name, MAX_OUTPUT_NAME, "%s", name);

external-mirrors/xorg-xserver

commit b096785df4bde7ff6a012d35fbbeb197f9734fff

xwayland: Fix search of duplicate lease names
Commit d36f66f15d ("Check for duplicate output names") would walk the
list of existing outputs and leases to check that no other existing
output has the same name.

The change however, inadvertently reused the regular screen outputs when
searching the leased names.

Fix this by using the lease name, not the regular output names that we
already checked just above.

Found by SAST tool Svace.

Fixes: d36f66f15d - xwayland: Check for duplicate output names
Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1843
Reviewed-by Olivier Fourdan <ofourdan@redhat.com>

Signed-off-by: Mikhail Dmitrichenko <m.dmitrichenko222@gmail.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2063>

hw/xwayland/xwayland-output.c
@@ -721,7 +721,7 @@ xwl_output_set_name(struct xwl_output *xwl_output, const char *name)
     /* And leases' names as well */
     xorg_list_for_each_entry(lease, &pScrPriv->leases, list) {
         for (i = 0; i < lease->numOutputs; i++) {
-            if (!strcmp(name, pScrPriv->outputs[i]->name)) {
+            if (!strcmp(name, lease->outputs[i]->name)) {
                 ErrorF("A lease output named '%s' already exists", name);
                 return;
             }