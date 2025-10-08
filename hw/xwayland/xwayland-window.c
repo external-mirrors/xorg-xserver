letoram/xarcan

commit a4ed100c0cb8cb35a2e4dd837cd9eea80d2345f4

xwayland: Set wl_surface input region
Some applications that use client side decorations usually set custom
input shape in order to prevent drop shadows stealing pointer events
from windows below. Currently, the only way to get it is to use some
XFixes APIs.

On the other hand, plenty of wayland compositors use solely the
wl_surface input region to decide what view can receive pointer input,
which results in some pointer input issues around client side drop
shadows because Xwayland doesn't set wl_surface.input_region.

See-also: https://bugs.kde.org/show_bug.cgi?id=448119
Signed-off-by: Vlad Zahorodnii <vlad.zahorodnii@kde.org>
Reviewed-by: Olivier Fourdan <ofourdan@redhat.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1510>

hw/xwayland/xwayland-window.c
@@ -1502,6 +1502,8 @@ ensure_surface_for_window(WindowPtr window)
             xwl_screen->tearing_control_manager, xwl_window->surface);
     }
 
+    xwl_window_set_input_region(xwl_window, wInputShape(window));
+
     return xwl_window;
 
 err:
@@ -1969,6 +1971,34 @@ xwl_window_post_damage(struct xwl_window *xwl_window)
 
     xwl_window_create_frame_callback(xwl_window);
     DamageEmpty(window_get_damage(xwl_window->surface_window));
+}
+
+void
+xwl_window_set_input_region(struct xwl_window *xwl_window,
+                            RegionPtr input_shape)
+{
+    struct wl_region *region;
+    BoxPtr box;
+    int i;
+
+    if (!input_shape) {
+        wl_surface_set_input_region(xwl_window->surface, NULL);
+        return;
+    }
+
+    region = wl_compositor_create_region(xwl_window->xwl_screen->compositor);
+    box = RegionRects(input_shape);
+
+    for (i = 0; i < RegionNumRects(input_shape); ++i, ++box) {
+        wl_region_add(region,
+                      box->x1,
+                      box->y1,
+                      box->x2 - box->x1,
+                      box->y2 - box->y1);
+    }
+
+    wl_surface_set_input_region(xwl_window->surface, region);
+    wl_region_destroy(region);
 }
 
 Bool

hw/xwayland/xwayland-screen.c
@@ -332,6 +332,32 @@ xwl_cursor_warped_to(DeviceIntPtr device,
     xwl_seat_emulate_pointer_warp(xwl_seat, xwl_window, sprite, x, y);
 }
 
+static void
+xwl_set_shape(WindowPtr window,
+              int kind)
+{
+    ScreenPtr screen = window->drawable.pScreen;
+    struct xwl_screen *xwl_screen;
+    struct xwl_window *xwl_window;
+
+    xwl_screen = xwl_screen_get(screen);
+    xwl_window = xwl_window_from_window(window);
+
+    screen->SetShape = xwl_screen->SetShape;
+    (*screen->SetShape) (window, kind);
+    xwl_screen->SetShape = screen->SetShape;
+    screen->SetShape = xwl_set_shape;
+
+    if (!xwl_window)
+        return;
+
+    if (kind == ShapeInput) {
+        xwl_window_set_input_region(xwl_window, wInputShape(window));
+        if (xwl_window->allow_commits)
+            wl_surface_commit(xwl_window->surface);
+    }
+}
+
 static struct xwl_window *
 find_matching_input_output_window(struct xwl_screen *xwl_screen,
                                   WindowPtr window)
@@ -1124,6 +1150,9 @@ xwl_screen_init(ScreenPtr pScreen, int argc, char **argv)
 
     xwl_screen->SetWindowPixmap = pScreen->SetWindowPixmap;
     pScreen->SetWindowPixmap = xwl_window_set_window_pixmap;
+
+    xwl_screen->SetShape = pScreen->SetShape;
+    pScreen->SetShape = xwl_set_shape;
 
     pScreen->CursorWarpedTo = xwl_cursor_warped_to;
     pScreen->CursorConfinedTo = xwl_cursor_confined_to;

hw/xwayland/xwayland-screen.h
@@ -80,6 +80,7 @@ struct xwl_screen {
     ChangeWindowAttributesProcPtr ChangeWindowAttributes;
     MoveWindowProcPtr MoveWindow;
     SourceValidateProcPtr SourceValidate;
+    SetShapeProcPtr SetShape;
 
     int (*GrabServer) (ClientPtr client);
     int (*UngrabServer) (ClientPtr client);

hw/xwayland/xwayland-window.h
@@ -143,6 +143,7 @@ Bool xwl_destroy_window(WindowPtr window);
 void xwl_window_post_damage(struct xwl_window *xwl_window);
 void xwl_window_create_frame_callback(struct xwl_window *xwl_window);
 void xwl_window_surface_do_destroy(struct xwl_wl_surface *xwl_wl_surface);
+void xwl_window_set_input_region(struct xwl_window *xwl_window, RegionPtr input_shape);
 
 Bool xwl_window_init(void);
 

external-mirrors/xorg-xserver

commit 66984ab8ae6eddd1a8246555d4aa530c96f6c0ad

xwayland: Take viewport scale into account for the input region
This is necessary because the input region is specified in surface
coordinates.

Fixes: a4ed100c0cb8 ("xwayland: Set wl_surface input region:)
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2071>

hw/xwayland/xwayland-window.c
@@ -394,6 +394,7 @@ xwl_window_disable_viewport(struct xwl_window *xwl_window)
     xwl_window->viewport = NULL;
     xwl_window->viewport_scale_x = 1.0;
     xwl_window->viewport_scale_y = 1.0;
+    xwl_window_set_input_region(xwl_window, wInputShape(xwl_window->toplevel));
 }
 
 /* Enable the viewport for fractional scale support with Xwayland rootful.
@@ -430,6 +431,7 @@ xwl_window_enable_viewport_for_fractional_scale(struct xwl_window *xwl_window,
 
     xwl_window->viewport_scale_x = scale;
     xwl_window->viewport_scale_y = scale;
+    xwl_window_set_input_region(xwl_window, wInputShape(xwl_window->toplevel));
 }
 
 /* Enable the viewport for Xwayland rootful fullscreen, to match the XRandR
@@ -465,6 +467,7 @@ xwl_window_enable_viewport_for_output(struct xwl_window *xwl_window,
 
     xwl_window->viewport_scale_x = (float) width / xwl_output->width;
     xwl_window->viewport_scale_y = (float) height / xwl_output->height;
+    xwl_window_set_input_region(xwl_window, wInputShape(xwl_window->toplevel));
 }
 
 static Bool
@@ -2039,12 +2042,20 @@ xwl_window_set_input_region(struct xwl_window *xwl_window,
     region = wl_compositor_create_region(xwl_window->xwl_screen->compositor);
     box = RegionRects(input_shape);
 
-    for (i = 0; i < RegionNumRects(input_shape); ++i, ++box) {
-        wl_region_add(region,
-                      box->x1,
-                      box->y1,
-                      box->x2 - box->x1,
-                      box->y2 - box->y1);
+    for (i = 0; i < RegionNumRects(input_shape); ++i) {
+        BoxRec b = box[i];
+
+        if (xwl_window->viewport_scale_x != 1.0f) {
+            b.x1 = floorf(b.x1 / xwl_window->viewport_scale_x);
+            b.x2 = ceilf(b.x2 / xwl_window->viewport_scale_x);
+        }
+
+        if (xwl_window->viewport_scale_y != 1.0f) {
+            b.y1 = floorf(b.y1 / xwl_window->viewport_scale_y);
+            b.y2 = ceilf(b.y2 / xwl_window->viewport_scale_y);
+        }
+
+        wl_region_add(region, b.x1, b.y1, b.x2 - b.x1, b.y2 - b.y1);
     }
 
     wl_surface_set_input_region(xwl_window->surface, region);