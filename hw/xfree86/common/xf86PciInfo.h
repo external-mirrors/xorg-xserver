external-mirrors/xorg-xserver

commit e8f45223120eb2dd280a8fe87fcde21895201689

Fix typos
Signed-off-by: Diego Viola <diego.viola@gmail.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2134>

hw/xfree86/common/xf86PciInfo.h
@@ -362,7 +362,7 @@
 /* ASPEED Technology (AST) */
 #define PCI_CHIP_AST2000		0x2000
 
-/* Avance Logic */
+/* Advance Logic */
 #define PCI_CHIP_ALG2064		0x2064
 #define PCI_CHIP_ALG2301		0x2301
 #define PCI_CHIP_ALG2501		0x2501

commit 67c82078febb8ab404c3503db123f4f28e75c4ab

Restore correct spelling of "Avance Logic"
Fixes: e8f452231 ("Fix typos")
Signed-off-by: Diego Viola <diego.viola@gmail.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2136>

hw/xfree86/common/xf86PciInfo.h
@@ -362,7 +362,7 @@
 /* ASPEED Technology (AST) */
 #define PCI_CHIP_AST2000		0x2000
 
-/* Advance Logic */
+/* Avance Logic */
 #define PCI_CHIP_ALG2064		0x2064
 #define PCI_CHIP_ALG2301		0x2301
 #define PCI_CHIP_ALG2501		0x2501