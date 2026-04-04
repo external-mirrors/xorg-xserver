external-mirrors/xorg-xserver

commit 79d39eabd44b5a98b37c973f470f9e3e30b922e2

xfree86: platform_noop: add missing functions
In order to be able to build on Linux w/ platform_noop
(when no recent enough libdrm found), a few more dummy functions
are needed here.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1848>
(cherry picked from commit e2ac4616290ed9b67a201465fafa0ab4e2b00a4b)

hw/xfree86/os-support/shared/platform_noop.c
@@ -3,6 +3,8 @@
 #include <xorg-config.h>
 #endif
 
+#include "config/hotplug_priv.h"
+
 #ifdef XSERVER_PLATFORM_BUS
 /* noop platform device support */
 #include "xf86_OSproc.h"
@@ -18,6 +20,18 @@ xf86PlatformDeviceCheckBusID(struct xf86_platform_device *device, const char *bu
 
 void xf86PlatformDeviceProbe(struct OdevAttributes *attribs)
 {
-
 }
+
+void xf86PlatformReprobeDevice(int index, struct OdevAttributes *attribs)
+{
+}
+
+void DeleteGPUDeviceRequest(struct OdevAttributes *attribs)
+{
+}
+
+void NewGPUDeviceRequest(struct OdevAttributes *attribs)
+{
+}
+
 #endif

commit a174db79e96b390aaf5e38f87fbc64ac6e4755d1

xf86: fix hotplug header include in platform_noop.c
In this branch, only hotplug.h exists, not hotplug_priv.h

Fixes: 79d39eabd ("xfree86: platform_noop: add missing functions")
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2157>

hw/xfree86/os-support/shared/platform_noop.c
@@ -3,14 +3,14 @@
 #include <xorg-config.h>
 #endif
 
-#include "config/hotplug_priv.h"
-
 #ifdef XSERVER_PLATFORM_BUS
 /* noop platform device support */
 #include "xf86_OSproc.h"
 
 #include "xf86.h"
 #include "xf86platformBus.h"
+
+#include "hotplug.h"
 
 Bool
 xf86PlatformDeviceCheckBusID(struct xf86_platform_device *device, const char *busid)