external-mirrors/xorg-xserver

commit 5967a88f0f1d205390070fd0cac7b0e4cddcfb70

Add AllowForceTerminate to xorg.conf
Forced termination is only allowed on bare hardware, not when running in
another window system.

Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/719>

hw/xfree86/common/xf86Config.c
@@ -66,6 +66,7 @@
 #include "xf86Xinput.h"
 #include "loaderProcs.h"
 
+#include "misc.h"
 #include "xkbsrv.h"
 #include "picture.h"
 #ifdef DPMSExtension
@@ -625,6 +626,7 @@ typedef enum {
     FLAG_DONTVTSWITCH,
     FLAG_DONTZAP,
     FLAG_DONTZOOM,
+    FLAG_ALLOW_FORCE_TERMINATE,
     FLAG_DISABLEVIDMODE,
     FLAG_ALLOWNONLOCAL,
     FLAG_ALLOWMOUSEOPENFAIL,
@@ -657,6 +659,8 @@ typedef enum {
  * if the parser found the option in the config file.
  */
 static OptionInfoRec FlagOptions[] = {
+    {FLAG_DONTVTSWITCH, "AllowForceTerminate", OPTV_BOOLEAN,
+     {0}, FALSE},
     {FLAG_DONTVTSWITCH, "DontVTSwitch", OPTV_BOOLEAN,
      {0}, FALSE},
     {FLAG_DONTZAP, "DontZap", OPTV_BOOLEAN,
@@ -727,6 +731,12 @@ configServerFlags(XF86ConfFlagsPtr flagsconf, XF86OptionPtr layoutopts)
     const char *rules;
 
     /*
+     * Allow force terminate by default when running on hardware.
+     * Other DDXs do not set this.
+     */
+    allowForceTerminate = TRUE;
+
+    /*
      * Merge the ServerLayout and ServerFlags options.  The former have
      * precedence over the latter.
      */
@@ -746,6 +756,7 @@ configServerFlags(XF86ConfFlagsPtr flagsconf, XF86OptionPtr layoutopts)
     xf86GetOptValBool(FlagOptions, FLAG_DONTVTSWITCH, &xf86Info.dontVTSwitch);
     xf86GetOptValBool(FlagOptions, FLAG_DONTZAP, &xf86Info.dontZap);
     xf86GetOptValBool(FlagOptions, FLAG_DONTZOOM, &xf86Info.dontZoom);
+    xf86GetOptValBool(FlagOptions, FLAG_ALLOW_FORCE_TERMINATE, &allowForceTerminate);
 
     xf86GetOptValBool(FlagOptions, FLAG_IGNORE_ABI, &xf86Info.ignoreABI);
     if (xf86Info.ignoreABI) {

include/misc.h
@@ -406,6 +406,9 @@ extern _X_EXPORT unsigned long serverGeneration;
 /* Stop all event delivery to clients effective immediately. */
 extern Bool stopAllEventDelivery;
 
+/* Allow clients to force the X server to terminate */
+extern Bool allowForceTerminate;
+
 /* Don't use this directly, use BUG_WARN or BUG_WARN_MSG instead */
 #define __BUG_WARN_MSG(cond, with_msg, ...)                                \
           do { if (cond) {                                                \

xfixes/disconnect.c
@@ -61,6 +61,7 @@ typedef struct _ClientDisconnect {
 #define GetClientDisconnect(s) \
     ((ClientDisconnectPtr) dixLookupPrivate(&(s)->devPrivates, \
                                             ClientDisconnectPrivateKey))
+Bool allowForceTerminate;
 
 int
 ProcXFixesSetClientDisconnectMode(ClientPtr client)
@@ -79,6 +80,8 @@ ProcXFixesSetClientDisconnectMode(ClientPtr client)
      * and have permission to manage the server.
      */
     if (stuff->disconnect_mode & XFixesClientDisconnectFlagForceTerminate) {
+        if (!allowForceTerminate)
+            return BadAccess;
         if (!ClientIsLocal(client))
             return BadAccess;
         if ((rc = XaceHookServerAccess(client, DixManageAccess)))

commit 11bc81f6548485c612a6217539fe90db315c035b

xfree86: correct flag set by AllowForceTerminate option
Fixes: 5967a88f0 ("Add AllowForceTerminate to xorg.conf") from !719
Reported-by: @stefan11111
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2258>

hw/xfree86/common/xf86Config.c
@@ -659,7 +659,7 @@ typedef enum {
  * if the parser found the option in the config file.
  */
 static OptionInfoRec FlagOptions[] = {
-    {FLAG_DONTVTSWITCH, "AllowForceTerminate", OPTV_BOOLEAN,
+    {FLAG_ALLOW_FORCE_TERMINATE, "AllowForceTerminate", OPTV_BOOLEAN,
      {0}, FALSE},
     {FLAG_DONTVTSWITCH, "DontVTSwitch", OPTV_BOOLEAN,
      {0}, FALSE},