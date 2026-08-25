f30461a7d4f7cf9091e893412eb01241e7a08e1e
Lukáš Lipinský <18076-Mr-Tao@users.noreply.gitlab.freedesktop.org>
2026-08-26 01:08:28 +0200

    Xi: byte-swap XIQueryDevice ScrollClass flags
    SwapScrollInfo converted the other multi-byte ScrollClass fields but left flags in server byte order. Swap it as well and decode the field in the swapped-client protocol test so nonzero flags exercise the wire representation.
    
    Signed-off-by: Lukáš Lipinský <18076-Mr-Tao@users.noreply.gitlab.freedesktop.org>
    (cherry picked from commit 8033c4d93ea140025fea5e7294bb4e92c0cd6af5)
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2310>

--- test/xi2/protocol-xiquerydevice.c
+++ test/xi2/protocol-xiquerydevice.c
@@ -202,6 +202,7 @@ reply_XIQueryDevice_data(ClientPtr client, int len, void *data)
                     if (client->swapped) {
                         swaps(&si->number);
                         swaps(&si->scroll_type);
+                        swapl(&si->flags);
                         swapl(&si->increment.integral);
                         swapl(&si->increment.frac);
                     }
