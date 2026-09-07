external-mirrors/xorg-xserver

commit c84beb1a48729a774d17a5fc7b0db565efd2296e

xnest/mi: remove redundant call of miScreenDevPrivateInit()
miScreenDevPrivateInit() is also made static in this commit.

Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/854>
(cherry picked from commit aa3f5023e3fae0df74039702b6c8218bc14dc679)

hw/xnest/Screen.c
@@ -242,8 +242,9 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
 
     /* myNum */
     /* id */
-    miScreenInit(pScreen, NULL, xnestWidth, xnestHeight, 1, 1, xnestWidth, rootDepth, numDepths, depths, defaultVisual, /* root visual */
-                 numVisuals, visuals);
+    if (!miScreenInit(pScreen, NULL, xnestWidth, xnestHeight, 1, 1, xnestWidth, rootDepth, numDepths, depths, defaultVisual, /* root visual */
+                      numVisuals, visuals))
+        return FALSE;
 
     pScreen->defColormap = (Colormap) FakeClientID(0);
     pScreen->minInstalledCmaps = MINCMAPS;
@@ -331,9 +332,6 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
 
     /* overwrite miCloseScreen with our own */
     pScreen->CloseScreen = xnestCloseScreen;
-
-    if (!miScreenDevPrivateInit(pScreen, xnestWidth, NULL))
-        return FALSE;
 
     /* overwrite miSetShape with our own */
     pScreen->SetShape = xnestSetShape;

mi/mi.h
@@ -382,11 +382,6 @@ extern _X_EXPORT Bool miModifyPixmapHeader(PixmapPtr pPixmap,
 extern _X_EXPORT Bool miCreateScreenResources(ScreenPtr /*pScreen */
     );
 
-extern _X_EXPORT Bool miScreenDevPrivateInit(ScreenPtr pScreen,
-                                             int width,
-                                             void *pbits
-    );
-
 extern _X_EXPORT Bool miScreenInit(ScreenPtr pScreen,
                                    void *pbits,
                                    int xsize,

mi/miscrinit.c
@@ -184,7 +184,7 @@ miCreateScreenResources(ScreenPtr pScreen)
     return TRUE;
 }
 
-Bool
+static Bool
 miScreenDevPrivateInit(ScreenPtr pScreen, int width, void *pbits)
 {
     miScreenInitParmsPtr pScrInitParms;

commit 14981eea447d1ee6624d7ca6644061a605dcae1c

Xnest: do not reset pScreen->devPrivate to NULL
Regression caused by c84beb1a48729a774d17a5fc7b0db565efd2296e
xnest/mi: remove redundant call of miScreenDevPrivateInit()
It is now initialized earlier by miScreenInit().

Signed-off-by: Matthieu Herrb <matthieu@herrb.eu>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2314>

hw/xnest/Screen.c
@@ -257,7 +257,6 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
     pScreen->blackPixel = xnestBlackPixel;
     /* GCperDepth */
     /* defaultStipple */
-    pScreen->devPrivate = NULL;
     /* WindowPrivateLen */
     /* WindowPrivateSizes */
     /* totalWindowSize */